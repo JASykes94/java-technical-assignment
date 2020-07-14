72f3d887ad8709d0a45853ff62fd73cd8c7231fb
james <jamessykes94@gmail.com>
2020-07-14 11:03:12 +0100

    Create discount interface and two classes for different discounts

---
+++ src/main/java/kata/supermarket/BuyOneGetOneFreeDiscount.java
@@ -0,0 +1,13 @@
+package kata.supermarket;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class BuyOneGetOneFreeDiscount implements Discount
+{
+    @Override
+    public BigDecimal discountPrice(List<Item> items)
+    {
+        return null;
+    }
+}
---
+++ src/main/java/kata/supermarket/OneKilogramHalfPriceDiscount.java
@@ -0,0 +1,13 @@
+package kata.supermarket;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class OneKilogramHalfPriceDiscount implements Discount
+{
+    @Override
+    public BigDecimal discountPrice(List<Item> items)
+    {
+        return null;
+    }
+}
